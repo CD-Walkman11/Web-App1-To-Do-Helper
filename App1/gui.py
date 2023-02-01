228dec13e17bdef103b09ffb0b0921a60fd16e44
DCW <walkerdc11@gmail.com>
2023-02-01 14:33:26 -0500

    Changed theme, Added clock, Handled Edit and Complete error Day 18 02/01/2023

--- App1/gui.py
+++ App1/gui.py
@@ -1,17 +1,23 @@
 import functions as fnc
 import PySimpleGUI as sg
+import time
 
-label = sg.Text("Enter a To-Do")
-input_box = sg.InputText(tooltip="Enter To-Do", key='todo')
-add_button = sg.Button('Add')
+sg.theme('LightGrey1')
 
+clock = sg.Text('', key='clock')
+label1 = sg.Text("Enter a To-Do")
+label2 = sg.Text("To-Do List")
+input_box = sg.InputText(tooltip="Enter To-Do", key='todo', size=45)
+add_button = sg.Button('Add', size=10)
 list_box = sg.Listbox(values=fnc.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
-edit_button = sg.Button('Edit')
-complete_button = sg.Button('Complete')
-exit_button = sg.Button('Exit')
+edit_button = sg.Button('Edit', size=9)
+complete_button = sg.Button('Complete', size=9)
+exit_button = sg.Button('Exit', size=9)
 
-layout = [[label, input_box, add_button],
+layout = [[clock],
+          [label1], [input_box, add_button],
+          [label2],
           [list_box, edit_button, complete_button],
           [exit_button]]
 
@@ -20,10 +26,8 @@ window = sg.Window('My To-Do Helper',
                    font=('Helvetica', 12))
 
 while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values['todos'])
+    event, values = window.read(timeout=200)
+    window['clock'].update(value=time.strftime("%b %d, %Y | %I:%M:%S"))
     match event:
         case 'Add':
             todos = fnc.get_todos()
@@ -32,21 +36,27 @@ while True:
             fnc.write_todos(todos)
             window['todos'].update(values=todos)
         case 'Edit':
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
 
-            todos = fnc.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            fnc.write_todos(todos)
-            window['todos'].update(values=todos)
+                todos = fnc.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                fnc.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                sg.popup("Please select an item first.", font=('Helvetica', 12))
         case 'Complete':
-            todo_to_complete = values['todos'][0]
-            todos = fnc.get_todos()
-            todos.remove(todo_to_complete)
-            fnc.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = fnc.get_todos()
+                todos.remove(todo_to_complete)
+                fnc.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Please select an item first.", font=('Helvetica', 12))
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case 'Exit':
