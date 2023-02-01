076241d6f95c4b91335ea26a60ed465270e2c436
DCW <walkerdc11@gmail.com>
2023-02-01 14:00:38 -0500

    Implemented Complete and Exit Buttons, Day 17 01/30/2023

--- App1/gui.py
+++ App1/gui.py
@@ -8,10 +8,15 @@ add_button = sg.Button('Add')
 list_box = sg.Listbox(values=fnc.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button('Edit')
+complete_button = sg.Button('Complete')
+exit_button = sg.Button('Exit')
+
+layout = [[label, input_box, add_button],
+          [list_box, edit_button, complete_button],
+          [exit_button]]
 
 window = sg.Window('My To-Do Helper',
-                   layout=[[label, input_box, add_button],
-                           [list_box, edit_button]],
+                   layout=layout,
                    font=('Helvetica', 12))
 
 while True:
@@ -35,10 +40,18 @@ while True:
             todos[index] = new_todo
             fnc.write_todos(todos)
             window['todos'].update(values=todos)
+        case 'Complete':
+            todo_to_complete = values['todos'][0]
+            todos = fnc.get_todos()
+            todos.remove(todo_to_complete)
+            fnc.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
         case 'todos':
             window['todo'].update(value=values['todos'][0])
+        case 'Exit':
+            break
         case sg.WIN_CLOSED:
             break
-
 
 window.close()
