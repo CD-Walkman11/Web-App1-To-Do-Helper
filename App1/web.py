b2c7024de7052a6ca6956538d761dfe7f48f0716
DCW <walkerdc11@gmail.com>
2023-02-02 15:11:38 -0500

    Implemented "Add new todo items" feature Day 19 02/02/2023

--- App1/web.py
+++ App1/web.py
@@ -3,6 +3,13 @@ import functions as fnc
 
 todos = fnc.get_todos()
 
+
+def add_todo():
+    new_todo = st.session_state['new_todo'] + '\n'
+    todos.append(new_todo)
+    fnc.write_todos(todos)
+
+
 st.title("To-Do List Helper")
 st.subheader("A Productivity List Creator")
 st.write("What would you like to do today?")
@@ -10,5 +17,5 @@ st.write("What would you like to do today?")
 for todo in todos:
     st.checkbox(todo)
 
-
-st.text_input(label='', placeholder="Enter a To-Do...")
+st.text_input(label='', placeholder="Enter a To-Do...",
+              on_change=add_todo, key='new_todo')
